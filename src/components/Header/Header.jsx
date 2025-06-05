0ed96466b2a829ceda83c662e34c189e4d6e7996
Kpuk <sonich.alexey@gmail.com>
2025-06-05 19:13:47 +0300

    v0.01

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,56 @@
 // src/components/Header.jsx
-import './header.css'
+import React from 'react';
+import './header.css';
 
 
-const Header = () => {
-  return (
-    <header className="header">
+
+
+const UserMenu = () => (
+  <div className="user-menu">
+    Привет, Алексей!
+  </div>
+);
+
+const GuestMenu = () => (
+  <div className="guest-menu">
+    <button>Войти</button>
+    <button>Регистрация</button>
+  </div>
+);
+
+const Header = ({ variant = 'start', isAuthenticated = false }) => {
+
+  const menu = isAuthenticated ? <UserMenu /> : <GuestMenu />;
+
+  if (variant === 'game') {
+    return (
+      <header className="header header-game-page">
         <div className="header-left">
-            Logo
+          Игровой Логотип
         </div>
         <div className="header-center">
-            text
+          Информация об игре
         </div>
         <div className="header-right">
-            buttons
+          {menu}
         </div>
+      </header>
+    );
+  }
+
+  return (
+    <header className="header header-start-page">
+      <div className="header-left">
+        <ul className="header-list-buttons">
+          <li><button className="header-button">Главная</button></li>
+          <li><button className="header-button">Форум</button></li>
+          <li><button className="header-button">Правила</button></li>
+        </ul>
+      </div>
+  
+      <div className="header-right">
+        {menu}
+      </div>
     </header>
   );
 };
