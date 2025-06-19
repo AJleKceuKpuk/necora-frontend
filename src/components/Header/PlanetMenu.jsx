7841987ad401955bc16d9b909efd89c9f7f100f5
Kpuk <sonich.alexey@gmail.com>
2025-06-19 13:48:53 +0300

    v0.07

--- src/components/Header/PlanetMenu.jsx
+++ src/components/Header/PlanetMenu.jsx
@@ -1,5 +1,4 @@
-import { useState, useRef, useEffect } from "react";
-import { Link } from 'react-router-dom';
+import { useState, useRef, useEffect, useCallback } from "react";
 import "./css/header.css";
 import icons from "../../images/images";
 
@@ -7,14 +6,13 @@ const PlanetMenu = () => {
     const dropdownRef = useRef(null);
     const [isOpen, setIsOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsOpen((prev) => !prev);
-    };
+    const toggleMenu = useCallback(() => {
+        setIsOpen(prev => !prev);
+    }, []);
 
     //Закрытие меню   
     useEffect(() => {
         const handleClickOutside = (event) => {
-            // Если клик вне области dropdown, закрываем меню
             if (dropdownRef.current && !dropdownRef.current.contains(event.target) && isOpen) {
                 toggleMenu();
             }
@@ -29,8 +27,8 @@ const PlanetMenu = () => {
 
 
     return (
-        <div className="planet-container no-select" onClick={toggleMenu}>
-            <div className="current-planet header-button " ref={dropdownRef}>
+        <div className="planet-container no-select" ref={dropdownRef} >
+            <div className="current-planet header-button " onClick={toggleMenu}>
                 <img src={icons.planet} alt="planet" />
                 <div className='planet' >Planet [1:11:11]</div>
             </div>
@@ -40,43 +38,74 @@ const PlanetMenu = () => {
                 <div className="planet-container-item">
                     <div className="planet-coordinates">[11:11:11]</div>
                     <div className="planet-details">
-                        <div className="planet-details-left header-button">
-                            
+                        <div className="planet-details-left header-button current-item-planet">
                             <div className="planet-details-info">
-                                <div className="img-container img-36">
-                                    <img src={icons.planet} alt="planet" />
+                                <div className="planet-details-tittle">
+                                    <div className="img-container img-36">
+                                        <img src={icons.planet} alt="planet" />
+                                    </div>
+                                    <div className="planet-details-name">Planet</div>
                                 </div>
-                                <div className="planet-details-name">Planet</div>
+
                                 <div className="planet-details-weaponry">
-                                    <div className="card-text">100k</div>
-                                    <div className="card-text">10k</div>
+
+                                    <div className="card-text">
+                                        <div className="img-container img-15">
+                                            <img src={icons.swords} alt="planet" />
+                                        </div>
+                                        <div>99K</div>
+                                    </div>
+
+                                    <div className="card-text">
+                                        <div className="img-container img-15">
+                                            <img src={icons.shield} alt="planet" />
+                                        </div>
+                                        <div>999M</div>
+                                    </div>
+
                                 </div>
-                                
+
                             </div>
                         </div>
                         <div className="planet-details-right header-button">
-                            <div className="planet-details-name">Moon</div>
                             <div className="planet-details-info">
-                                <div className="img-container img-36">
-                                    <img src={icons.moon} alt="moon" />
+
+
+                                <div className="planet-details-weaponry">
+
+                                    <div className="card-text">
+                                        <div className="img-container img-15">
+                                            <img src={icons.swords} alt="planet" />
+                                        </div>
+                                        <div>999B</div>
+                                    </div>
+
+                                    <div className="card-text">
+                                        <div className="img-container img-15">
+                                            <img src={icons.shield} alt="planet" />
+                                        </div>
+                                        <div>999T</div>
+                                    </div>
+
                                 </div>
-                                <div className="card-text">10k</div>
-                                <div className="card-text">10k</div>
+                                <div className="planet-details-tittle">
+                                    <div className="planet-details-name">Moon</div>
+                                    <div className="img-container img-36">
+                                        <img src={icons.moon} alt="planet" />
+                                    </div>
+
+                                </div>
                             </div>
+
                         </div>
                     </div>
                 </div>
 
 
-                <li>
-                    <ul>1 planet</ul>
-                    <ul>1 planet</ul>
-                    <ul>1 planet</ul>
-                    <ul>1 planet</ul>
-                </li>
+
             </div>
         </div>
-        
+
 
     );
 };
