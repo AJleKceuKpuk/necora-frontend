fe40cf73c46ea834031106de187735c9bc0563a8
Kpuk <sonich.alexey@gmail.com>
2025-08-28 16:56:44 +0300

    v29 start SCSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import RouteAuthListener from "./routes/RouteAuthListener";
 import Loading from "./pages/Loading/Loading";
 import { useAuth } from "./hooks/useAuth";
 import RoutesConfig from "./routes/RoutesConfig";
+import "./styles/styles.scss"
 
 const App = () => {
   const { isInitializing } = useAuth();
